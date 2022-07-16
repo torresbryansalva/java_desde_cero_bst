37098409dfda7f68f82923376e7b55548ead621b
Bryan Salva <salvatorresb@gmail.com>
2022-07-16 12:37:54 -0500

    primeros pasos en java, empezamos

---
+++ src/paso_por_valor/PasoPorValor.java
@@ -0,0 +1,19 @@
+package paso_por_valor;
+
+/**
+ *
+ * @author bryan salva torres
+ */
+public class PasoPorValor {
+    public static void main(String[] args) {
+        int x=12;
+        System.out.println("x="+x);
+        cambioValor(x);
+    }
+    
+    public static void cambioValor(int arg1){
+        System.out.println("el argumento:"+arg1+" este es el paso de valor de X");
+        arg1=11;
+        System.out.println("el argumente al final es:"+arg1);
+    }
+}
